7c007126a166cb4bb87b93f95a4b0748e6e97025
Enes Akkuş <116295097+enesvct@users.noreply.github.com>
2023-10-26 10:49:53 +0300

    cleaning

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TurhanFirin.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
